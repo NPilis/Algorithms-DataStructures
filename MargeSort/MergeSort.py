7f9c555acfb253a77fd56428fe08eacab096305e
Norbert <norbertwx19@wp.pl>
2020-01-26 15:10:11 +0100

    Implemented

--- MargeSort/MergeSort.py
+++ MargeSort/MergeSort.py
@@ -44,3 +44,40 @@ def mergeSort(tab,l,r):
 s1 = [1,4,5,2,3,8,7]
 mergeSort(s1,0,len(s1)-1)
 print(s1)
+
+
+def mergeSort2(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        L = arr[:mid]
+        R = arr[mid:]
+
+        mergeSort2(L)
+        mergeSort2(R)
+
+        i = j = k = 0
+
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+
+
+s2 = [1,4,5,2,3,8,7]
+mergeSort2(s2)
+print(s2)
