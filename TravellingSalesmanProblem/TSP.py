d61ec84858ecf33fc46fafb58840ac997693c718
Norbert <norbertwx19@wp.pl>
2020-03-18 23:36:19 +0100

    Little alghoritm implemented

--- TravellingSalesmanProblem/TSP.py
+++ TravellingSalesmanProblem/TSP.py
@@ -1,5 +1,12 @@
 import numpy as np
-from typing import Tuple
+
+"""
+Given a list of cities and the distances between each pair of cities, 
+what is the shortest possible route that visits each city and returns 
+to the origin city
+
+Little Algorithm approach
+"""
 
 # Forbidden path as infinity
 INF = np.inf
@@ -7,32 +14,33 @@ INF = np.inf
 class CostMatrix:
 
     def __init__(self, matrix):
-        self.costMatrix = matrix
-        self.rowSize, self.colSize = matrix.shape
-        self.lowerBound = 0
-        self.rowMap = {i:i+1 for i in range(self.rowSize)}
-        self.colMap = {i:i+1 for i in range(self.colSize)}
+        self.cost_matrix = matrix
+        self.row_size, self.col_size = matrix.shape
+        self.lower_bound = 0
+        self.row_map = {i: i+1 for i in range(self.row_size)}
+        self.col_map = {i: i+1 for i in range(self.col_size)}
 
-    def printMatrix(self) -> None:
-        print(self.costMatrix)
+    def printMatrix(self):
+        print(self.cost_matrix)
 
+    # Returns the lowest cost of travel
     def getLowerBound(self) -> float:
-        return self.lowerBound
+        return self.lower_bound
 
     # Checks if zero is present minimum once in every column and every row
     def checkForZeros(self) -> bool:
 
-        for r in range(self.rowSize):
+        for r in range(self.row_size):
             assert_zero_row = False
             assert_zero_col = False
-            for c in range(self.colSize):
-                if self.costMatrix[r][c] == 0:
+            for c in range(self.col_size):
+                if self.cost_matrix[r][c] == 0:
                     assert_zero_row = True
 
-                if self.costMatrix[c][r] == 0:
+                if self.cost_matrix[c][r] == 0:
                     assert_zero_col = True
 
-            if assert_zero_col == False or assert_zero_row == False:
+            if assert_zero_col is False or assert_zero_row is False:
                 return False
 
         return True
@@ -40,43 +48,42 @@ class CostMatrix:
     # Both functions finds minimum of each row/column, reduces their values and sums for the lowest path length
     def reduceRows(self) -> None:
 
-        row_minimas = self.costMatrix.min(axis=1)
-        self.costMatrix = self.costMatrix - np.vstack(row_minimas)
-        self.lowerBound += sum(row_minimas)
+        row_minimas = self.cost_matrix.min(axis=1)
+        self.cost_matrix = self.cost_matrix - np.vstack(row_minimas)
+        self.lower_bound += sum(row_minimas)
 
     def reduceCols(self) -> None:
 
-        col_minimas = self.costMatrix.min(axis=0)
-        self.costMatrix = self.costMatrix - col_minimas
-        self.lowerBound += sum(col_minimas)
+        col_minimas = self.cost_matrix.min(axis=0)
+        self.cost_matrix = self.cost_matrix - col_minimas
+        self.lower_bound += sum(col_minimas)
 
     # Functions find minimum in each row or each column except pair values
     def findMinimumInRow(self, row, col) -> float:
         if col == 0:
-            min = self.costMatrix[row][1]
+            min = self.cost_matrix[row][1]
         else:
-            min = self.costMatrix[row][0]
+            min = self.cost_matrix[row][0]
 
-        for c in range(1, self.colSize):
+        for c in range(1, self.col_size):
             if c == col:
                 continue
-            if self.costMatrix[row][c] < min:
-                min = self.costMatrix[row][c]
+            if self.cost_matrix[row][c] < min:
+                min = self.cost_matrix[row][c]
 
         return min
 
     def findMinimumInCol(self, row, col) -> float:
         if row == 0:
-            min = self.costMatrix[1][col]
+            min = self.cost_matrix[1][col]
         else:
-            min = self.costMatrix[0][col]
+            min = self.cost_matrix[0][col]
 
-        for r in range(1, self.rowSize):
+        for r in range(1, self.row_size):
             if r == row:
                 continue
-            if self.costMatrix[r][col] < min:
-                min = self.costMatrix[r][col]
-
+            if self.cost_matrix[r][col] < min:
+                min = self.cost_matrix[r][col]
 
         return min
 
@@ -85,80 +92,105 @@ class CostMatrix:
 
         pairs_indexes = []
 
-        for r in range(self.rowSize):
-            for c in range(self.colSize):
-                if self.costMatrix[r][c] == 0:
+        for r in range(self.row_size):
+            for c in range(self.col_size):
+                if self.cost_matrix[r][c] == 0:
                     pairs_indexes.append((r, c))
 
         return pairs_indexes
 
-    # Removes row and column at indexes and blocks reverse way
+    # Removes row and column at indexes and blocks reverse way if there is any
     def deleteAndBan(self,row,col):
 
-        if row in self.rowMap.keys():
-            if col in self.rowMap.keys():
-                self.costMatrix[col][row] = INF
+        if self.row_map[row] in self.col_map.values() and self.col_map[col] in self.row_map.values():
+            self.cost_matrix[col][row] = INF
 
-        self.costMatrix = np.delete(self.costMatrix,row,0)
-        self.costMatrix = np.delete(self.costMatrix,col,1)
+        self.cost_matrix = np.delete(self.cost_matrix, row, 0)
+        self.cost_matrix = np.delete(self.cost_matrix, col, 1)
 
+    # Modifies mapping when the column or row is deleted 1 -> 0 etc.
     def modifyMapping(self,row,col):
 
+        for i in range(row, self.row_size-1):
+            self.row_map[i] = self.row_map[i+1]
+
+        for j in range(col, self.col_size-1):
+            self.col_map[j] = self.col_map[j+1]
+
+        del self.row_map[self.row_size - 1]
+        del self.col_map[self.col_size - 1]
 
 
-class TSP_Solver:
+class SolverTSP:
 
-    def __init__(self,costMatrix):
-        self.costMatrix = costMatrix
+    def __init__(self,cost_matrix):
+        self.cost_matrix = cost_matrix
         self.solution = []
         self.cost = 0
-        self.iterationCount = self.costMatrix.rowSize
+        self.iteration_count = self.cost_matrix.row_size
 
     # Function search for pair indexes with maximum sum of row an col minimum and adds to solution
     def findNextPath(self):
 
-        pair_indexes = self.costMatrix.findPairsWithZero()
+        pair_indexes = self.cost_matrix.findPairsWithZero()
         temp_sum = 0
         max_sum_idx = 0
 
-        for idx,pair in enumerate(pair_indexes):
-            row_min = self.costMatrix.findMinimumInRow(pair[0],pair[1])
-            col_min = self.costMatrix.findMinimumInCol(pair[0],pair[1])
+        for idx, pair in enumerate(pair_indexes):
+            row_min = self.cost_matrix.findMinimumInRow(pair[0],pair[1])
+            col_min = self.cost_matrix.findMinimumInCol(pair[0],pair[1])
             if row_min + col_min > temp_sum:
                 temp_sum = row_min + col_min
                 max_sum_idx = idx
 
         return pair_indexes[max_sum_idx]
 
-    def addToSolution(self,node_pair):
-        self.solution.append((self.costMatrix.rowMap[node_pair[0]],self.costMatrix.colMap[node_pair[1]]))
+    # Adds pair to solution
+    def addToSolution(self, node_pair):
+        self.solution.append((self.cost_matrix.row_map[node_pair[0]], self.cost_matrix.col_map[node_pair[1]]))
 
+    # Reducing sizes as rows and columns are deleted
     def reduceSizes(self):
-        self.costMatrix.rowSize -= 1
-        self.costMatrix.colSize -= 1
-        self.iterationCount -= 1
+        self.cost_matrix.row_size -= 1
+        self.cost_matrix.col_size -= 1
+        self.iteration_count -= 1
 
+    # Organizes solution as a path
+    def organizeSolution(self):
+
+        organized_solution = [self.solution.pop(0)]
+        head = organized_solution[0][0]
+        tail = organized_solution[0][1]
+
+        while head != tail:
+            for idx, val in enumerate(self.solution):
+                if val[0] == tail:
+                    organized_solution.append(self.solution.pop(idx))
+                    tail = val[1]
+                    break
+
+        self.solution = organized_solution
+
+    # All function combined solves the problem
     def solveTSP(self):
 
-        while self.iterationCount > 1:
-            if self.costMatrix.checkForZeros():
+        while self.iteration_count > 1:
+
+            if self.cost_matrix.checkForZeros():
+
                 pair = self.findNextPath()
                 self.addToSolution(pair)
-                self.costMatrix.deleteAndBan(pair[0],pair[1])
-                self.costMatrix.modifyMapping(pair[0],pair[1])
+                self.cost_matrix.deleteAndBan(pair[0], pair[1])
+                self.cost_matrix.modifyMapping(pair[0], pair[1])
                 self.reduceSizes()
+
             else:
-                self.costMatrix.reduceRows()
-                self.costMatrix.reduceCols()
+                self.cost_matrix.reduceRows()
+                self.cost_matrix.reduceCols()
+
+        self.solution.append((self.cost_matrix.row_map[0], self.cost_matrix.col_map[0]))
+        self.organizeSolution()
+        self.cost = self.cost_matrix.lower_bound
 
 
 
-tempMatrix = np.array([[INF, 10, 8, 19, 12],
-                           [10, INF, 20, 6, 3],
-                           [8, 20, INF, 4, 2],
-                           [19, 6, 4, INF, 7],
-                           [12, 3, 2, 7, INF]], dtype=np.float)
-costMatrix = CostMatrix(tempMatrix)
-tsp = TSP_Solver(costMatrix)
-tsp.solveTSP()
-print(tsp.solution)
--- TravellingSalesmanProblem/testTSP.py
+++ TravellingSalesmanProblem/testTSP.py
@@ -3,19 +3,14 @@ from TSP import *
 
 class TestSolver(unittest.TestCase):
 
-    def setUp(self):
-        self.tempMatrix = np.array([[INF, 10, 8, 19, 12],
-                           [10, INF, 20, 6, 3],
-                           [8, 20, INF, 4, 2],
-                           [19, 6, 4, INF, 7],
-                           [12, 3, 2, 7, INF]], dtype=np.float)
-        self.costMatrix = CostMatrix(self.tempMatrix)
-        self.tsp = TSP_Solver(self.costMatrix)
-
-    def test_print(self):
-        self.tsp.costMatrix.printMatrix()
+    temp_matrix1 = np.array([[INF, 10, 8, 19, 12],
+                            [10, INF, 20, 6, 3],
+                            [8, 20, INF, 4, 2],
+                            [19, 6, 4, INF, 7],
+                            [12, 3, 2, 7, INF]], dtype=np.float)
+    cost_matrix = CostMatrix(temp_matrix1)
+    tsp = SolverTSP(cost_matrix)
 
     def test_solver(self):
         self.tsp.solveTSP()
-        self.tsp.costMatrix.printMatrix()
-        print(self.tsp.solution)
+        self.assertEqual(self.tsp.solution, [(1, 3), (3, 4), (4, 5), (5, 2), (2, 1)])
